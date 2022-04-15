4114d881b590c62e88a29ea1c82d08a3ae92ac87
myunbongs <myunbongs@naver.com>
2022-04-15 17:35:55 +0900

    Post create,update/User authenticate

--- blog_main/blog/urls.py
+++ blog_main/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('/<int:pk>/', views.PostDetail.as_view()),
     path('/category/<str:slug>/', views.categories_posts),
     path('/tag/<str:slug>/', views.tag_page),
+    path('/create_post/', views.PostCreate.as_view(), name='create_post'),
+    path('/update_post/', views.PostUpdate.as_view(), name='update_post'),
 ]
--- blog_main/blog/views.py
+++ blog_main/blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .models import Post, Category, Tag
 
@@ -23,6 +25,37 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin ,CreateView):
+    model = Post
+    #fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ['title', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user_is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    #fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    fields = ['title', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    template_name = 'blog/update_post.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user
+        if (current_user.is_authenticated and current_user == self.get_object().author) or current_user.is_superuser:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
 
 def categories_posts(request, slug):
     if slug=='no-category':
